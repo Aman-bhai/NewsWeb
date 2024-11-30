650bbe9906528d78da9af73b17f6b761dc65512c
Aman Soni <amansoni18860@gmail.com>
2024-11-30 21:22:32 +0530

    api name

--- src/app/category/[category]/page.js
+++ src/app/category/[category]/page.js
@@ -6,7 +6,7 @@ const page = ({ params }) => {
   let color=colscheme(params.category)
   return (
     <main className='bg-gray-100 dark:bg-gray-800 min-h-screen'>
-    <Container category={params.category} API={process.env.API_KEY} color={color} />
+    <Container category={params.category} API={process.env.NEXT_PUBLIC_API_KEY} color={color} />
     </main>
 )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Container from "./components/Container";
 export default function Home() {
   return (
     <main className="bg-gray-100 dark:bg-gray-800 min-h-screen">
-      <Container API={process.env.API_KEY} category={"general"} />
+      <Container API={process.env.NEXT_PUBLIC_API_KEY} category={"general"} />
     </main>
   );
 }
